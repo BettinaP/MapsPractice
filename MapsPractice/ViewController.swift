ca5f880227a9e6e4c04dba771b5b6f8093a024ab
BettinaP <bettinaprophete@gmail.com>
2018-02-27 22:21:36 -0500

    first commit

---
+++ MapsPractice/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MapsPractice
+//
+//  Created by Bettina on 2/27/18.
+//  Copyright © 2018 Bettina Prophete. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 40.7
+        let longitude : CLLocationDegrees = 74.0
+        let latDelta: CLLocationDegrees = 0.05 //zoom level of map
+        let longDelta: CLLocationDegrees = 0.05 //zoom  level of map, number of degrees difference along the longitude of the map that we can view
+        
+        let span:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)// combo of latDelta & longDelta for overall zoom level of map
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
